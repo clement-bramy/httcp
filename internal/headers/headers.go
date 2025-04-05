273a8fe232d269cefb622af808c1c6fa162bcdab
clement-bramy <clement.bramy@gmail.com>
2025-04-05 16:03:50 +1100

    handle key edge cases

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -11,6 +11,7 @@ type Headers map[string]string
 var (
 	InvalidHeaderLineParts     = errors.New("invalid header line: parts numbers")
 	InvalidKeyFormatWhitespace = errors.New("invalid header line: key has trailing whitespaces")
+	InvalidKeyFormatCharacters = errors.New("invalid header line: key has invalid characters")
 )
 
 func NewHeaders() Headers {
@@ -31,11 +32,40 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	parts := strings.SplitN(raw, ":", 2)
 
 	key := parts[0]
-	value := strings.TrimSpace(parts[1])
 	if strings.HasSuffix(key, " ") {
 		return 0, false, InvalidKeyFormatWhitespace
 	}
 
-	h[key] = value
+	key, err = getValidKey(parts[0])
+	if err != nil {
+		return 0, false, InvalidKeyFormatCharacters
+	}
+
+	value := strings.TrimSpace(parts[1])
+
+	h[strings.ToLower(key)] = value
 	return index + 2, false, nil
+}
+
+const allowedSpecialChar = "!#$%&'*+-.^_`|~"
+
+func getValidKey(key string) (string, error) {
+	for _, r := range key {
+		alpha := isAlpha(r)
+		digit := isDigit(r)
+		specs := strings.Contains(allowedSpecialChar, string(r))
+
+		if !alpha && !digit && !specs {
+			return "", InvalidKeyFormatCharacters
+		}
+	}
+	return strings.ToLower(key), nil
+}
+
+func isAlpha(r rune) bool {
+	return (r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z')
+}
+
+func isDigit(r rune) bool {
+	return r >= '0' && r <= '9'
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,7 +14,7 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 }
@@ -37,22 +37,22 @@ func TestValidHeaderWithWhitespaces(t *testing.T) {
 
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "application/json", headers["Content-Type"])
+	assert.Equal(t, "application/json", headers["content-type"])
 	assert.Equal(t, 40, n)
 	assert.False(t, done)
 }
 
 func TestValidTwoHeaderWithExistingHeaders(t *testing.T) {
 	headers := NewHeaders()
-	headers["Host"] = "localhost:43069"
-	headers["Content-Length"] = "233"
+	headers["host"] = "localhost:43069"
+	headers["content-length"] = "233"
 	data := []byte("Content-Type:    application/json\r\n\r\n")
 
 	n, done, err := headers.Parse(data)
 
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "application/json", headers["Content-Type"])
+	assert.Equal(t, "application/json", headers["content-type"])
 	assert.Equal(t, 35, n)
 	assert.False(t, done)
 }
@@ -77,7 +77,7 @@ func TestHeaderThenDoneValid(t *testing.T) {
 
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "application/json", headers["Content-Type"])
+	assert.Equal(t, "application/json", headers["content-type"])
 	assert.Equal(t, 31, n)
 	assert.False(t, done)
 
@@ -87,4 +87,20 @@ func TestHeaderThenDoneValid(t *testing.T) {
 	require.NotNil(t, headers)
 	assert.Equal(t, 2, n)
 	assert.True(t, done)
+}
+
+func TestKeyInvalidCharacters(t *testing.T) {
+	inputs := []string{
+		"{:value\r\n",
+		"H@st: localhost:42069\r\n",
+	}
+
+	for _, input := range inputs {
+		t.Run(input, func(t *testing.T) {
+			headers := NewHeaders()
+			_, _, err := headers.Parse([]byte(input))
+
+			require.ErrorIs(t, err, InvalidKeyFormatCharacters)
+		})
+	}
 }
