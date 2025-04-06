56e747d7fff3e0606e2629509472bc76f0feeeac
clement-bramy <clement.bramy@gmail.com>
2025-04-06 10:19:15 +1000

    print body for the tcplistener

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
 
+	"github.com/clement-bramy/httcp/internal/headers"
 	"github.com/clement-bramy/httcp/internal/request"
 )
 
@@ -30,12 +33,37 @@ func main() {
 			fmt.Printf("failed to parse incoming data: %v\n", err)
 			continue
 		}
-		fmt.Printf(
-			"Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n",
-			req.RequestLine.Method,
-			req.RequestLine.RequestTarget,
-			req.RequestLine.HttpVersion,
-		)
+
+		writeReqLine(os.Stdout, req.RequestLine)
+		writeHeaders(os.Stdout, req.Headers)
+		writeBody(os.Stdout, req.Body)
+
 		fmt.Println("connection has been closed..")
+	}
+}
+
+func writeReqLine(w io.Writer, rl request.RequestLine) {
+	fmt.Fprintf(
+		w,
+		"Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n",
+		rl.Method,
+		rl.RequestTarget,
+		rl.HttpVersion,
+	)
+}
+
+func writeHeaders(w io.Writer, h headers.Headers) {
+	fmt.Fprintln(w, "Headers:")
+	for key, value := range h {
+		fmt.Fprintf(w, "- %s: %s\n", key, value)
+	}
+
+}
+
+func writeBody(w io.Writer, body []byte) {
+	fmt.Fprintln(w, "Body:")
+
+	if len(body) > 0 {
+		fmt.Fprintf(w, "%s\n", string(body))
 	}
 }
